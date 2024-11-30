4c4a53157e90a6af9378bb1a03c2e0a31a7b18f4
topij <topi.jarvinen@gmail.com>
2024-11-30 21:19:13 +0200

    Old file clean up

--- old_tests/helpers/mock_llms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseMockLLM
-from .category_mock import CategoryMockLLM
-from .keyword_mock import KeywordMockLLM
-from .theme_mock import ThemeMockLLM
-
-__all__ = ["BaseMockLLM", "KeywordMockLLM", "CategoryMockLLM", "ThemeMockLLM"]
